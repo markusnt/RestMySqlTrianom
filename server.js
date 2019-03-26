f65291802c4fac8a3ab1653ba93e5c31badfaded
markusnt <markus_nathan@live.com>
2019-03-26 17:39:44 -0300

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const express = require('express'),
   bodyParser = require('body-parser');
   port = process.env.PORT || 1337;
 
+const cors = require('cors');
+
+var corsOptions = {
+    origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept', 'x-access-token'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
+
+  
 
 const mysql = require('mysql');
 // connection configurations
